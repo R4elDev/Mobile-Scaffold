46a873bf739a4ddd9e8ccc9193f5108965121ac2
ISRAEL MAGALHÃES DOS SANTOS JUNIOR <24122293@dsn127m1241954.cfp127aluno.local>
2025-08-05 12:27:25 -0300

    Fazendo mudanças/Criando Cards

--- app/src/main/java/com/example/clientesapp/screens/HomeScreen.kt
+++ app/src/main/java/com/example/clientesapp/screens/HomeScreen.kt
@@ -6,17 +6,25 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -29,6 +37,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -63,9 +72,72 @@ fun HomeScreen(modifier: Modifier = Modifier) {
                 .fillMaxSize()
                 .background(color = MaterialTheme.colorScheme.background)
         ) {
-            Text(text = "Aqui fica o conteudo")
+            Row(
+                modifier = Modifier
+                    .padding(16.dp)
+            ){
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = "Icone da lista de clientes",
+                    tint = MaterialTheme.colorScheme.onBackground
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = "Lista de clientes"
+                )
+            }
+
+            LazyColumn {
+                items(count = 10){
+                    ClientCard()
+                }
+            }
         }
 
+    }
+}
+
+@Composable
+fun ClientCard(modifier: Modifier = Modifier) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(70.dp)
+            .padding(
+                start = 8.dp,
+                end = 8.dp,
+                bottom = 8.dp
+            ),
+        colors = CardDefaults.cardColors(
+            contentColor = MaterialTheme.colorScheme.primary
+        )
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(86.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Column {
+                Text(text = "Nome do cliente")
+                Text(text = "Email do client")
+            }
+
+            Icon(
+                imageVector = Icons.Default.Delete,
+                contentDescription = "Excluir"
+            )
+        }
+
+    }
+}
+
+@Preview
+@Composable
+private fun ClientCardPreview() {
+    ClientesappTheme {
+        ClientCard()
     }
 }
 
@@ -194,6 +266,12 @@ fun BotaoCadastrar(modifier: Modifier = Modifier) {
         )
     }
 }
+
+
+
+
+
+
 
 @Preview
 @Composable
